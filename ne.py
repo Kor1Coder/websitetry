5621fea845434752798f84ae8ff1d1d19e6dbb9c
asus <huba.huba_06@hotma>
2023-09-03 17:43:32 +0300

    adding secon commnet

---
+++ ne.py
@@ -0,0 +1,11 @@
+import streamlit as st
+col1,col2=st.columns(2)
+with col1:
+    st.image("image/Background/Blue.png",width=300)
+
+with col2:
+    st.title('ALİ YILMAZ')
+    content='hi all I am Ali.hi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Alihi all I am Ali'
+    st.info(content)
+t=('hi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali i am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali Yılmazhi am I Ali ')
+st.info(t)
